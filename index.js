5bd2a26e0a717440b04af89fc74ed2ec4ecec1cb
sidney mulwa <mulwasidney@outlook.com>
2022-06-21 10:54:21 +0300

    lab completed

---
+++ index.js
@@ -0,0 +1,15 @@
+function receivesAFunction(callBack){
+    callBack();
+   }
+   
+   function returnsANamedFunction(){
+       return function named(){
+           console.log(`This is a named function!`);
+       }
+   }
+   
+   function returnsAnAnonymousFunction(){
+       return function (){
+           console.log(`This is an anonymous function`);
+       };
+   } 
